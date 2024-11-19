4adc96e481ff7c1354e67722ada1c7a35c62c1f5
yaron <yaron@YARONIX>
2024-11-19 20:02:14 +0200

    amen a

--- TripPlanServer/ModelsDB/TripPlanDbContext.cs
+++ TripPlanServer/ModelsDB/TripPlanDbContext.cs
@@ -11,7 +11,7 @@ public partial class TripPlanDbContext : DbContext
         return this.Users.Where(u => u.Email == email).FirstOrDefault();
     }
 
-    public List<PlanGroup> GetAllPlanningsByEmail(string email)
+    public List<PlanGroup>? GetAllPlanningsByEmail(string email)
     {
         return this.PlanGroups.Where(pg => pg.Users.Any(u => u.Email == email)).ToList();
     }
